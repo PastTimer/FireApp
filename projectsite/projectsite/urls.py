3eab8648e2a9781e4df85767e22f10c15583c2f5
Imee <vitoimee@gmail.com>
2025-04-13 21:37:30 +0800

    Dynamic Multibar Chart

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 
-from fire.views import HomePageView, ChartView, PieCountbySeverity, LineCountbyMonth, MultilineIncidentTop3Country
+from fire.views import HomePageView, ChartView, PieCountbySeverity, LineCountbyMonth, MultilineIncidentTop3Country, multipleBarBySeverity
 from fire import views
 
 urlpatterns = [
@@ -11,6 +11,7 @@ urlpatterns = [
     path('chart/', PieCountbySeverity, name='chart'),
     path('lineChart/', LineCountbyMonth, name='chart'),
     path('multilineChart/', MultilineIncidentTop3Country, name='chart'),
+    path('multiBarchart/', multipleBarBySeverity, name='chart'),
 
 ]
 
